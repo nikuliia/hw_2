65ca103af1c5609c8e55ab780350903c90ac0f83
nikuliia <iuliianiko@gmail.com>
2025-01-19 21:38:24 +0100

    commit 4

--- task_4_2/main4.2.js
+++ task_4_2/main4.2.js
@@ -1,8 +1,31 @@
 "use strict"
 
-let threeDigitNumber = prompt('123');
-const arr = Array.from(String(threeDigitNumber), Number);
-console.log(arr);
+run()
 
-const sameItems = arr.every((item) => item === arr[0]);
-console.log(sameItems ? 'true' : 'false');
+function run() {
+    const value = prompt("Please, type a number from 100 to 999")
+    const number = +value
+    if (isNaN(number)) {
+        alert("Not a number. Please, reload the page and try again!")
+        return
+    }
+
+    if (number < 100 || number > 999) {
+        alert("Not a number. Please, reload page and try again!")
+        return
+    }
+
+    const numbers = value.split('')
+    const unique = new Set(numbers)
+
+    const isAllEqual = unique.size === 1,
+        isSomeEqual = unique.size === 2
+
+    if (isAllEqual) {
+        alert("All numbers is equals.")
+    }
+
+    if (isSomeEqual) {
+        alert("Some numbers is equals.")
+    }
+}
